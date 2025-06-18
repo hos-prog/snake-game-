87eeaae748ebb3a749eb1ecbcf1999ac64067043
hos <hosamaadem8@gmail.com>
2025-06-19 00:32:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle,Screen
+from scorboard import Scoreboard
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("hosama's snake game")
 screen.tracer(0)
 
 snake = Snake()
+food=Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,6 +27,24 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    #Detect the collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()<-280 or snake.head.ycor()>280:
+        game_is_on=False
+        scoreboard.game_over()
+
+    #detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on=False
+            scoreboard.game_over()
+
 
 
 screen.exitonclick()
